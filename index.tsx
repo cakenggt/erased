0cca0c984a3c4abdd9abab489b2611ce33310786
Alec Lownes <alec.lownes@gmail.com>
2018-05-31 22:53:48 -0400

    Finish transferring messages over

--- index.tsx
+++ index.tsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 
 import messages from './messages';
 
-class HelloMessage extends React.Component<{ name: string }> {
+class HelloMessage extends React.Component {
   render() {
-    return (
-      <div>
-        Hello {this.props.name} {JSON.stringify(messages)}
-      </div>
-    );
+    return <div>{JSON.stringify(messages)}</div>;
   }
 }
 
 var mountNode = document.getElementById('app');
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+ReactDOM.render(<HelloMessage />, mountNode);
--- messages.ts
+++ messages.ts
@@ -14,8 +14,9 @@ export type Messages = Record<string, Message[]>;
 const parsed: Messages = {};
 
 export default Object.keys(messages).reduce((acc, channel) => {
-  acc[channel] = messages[channel].map(({ date, ...rest }) => ({
+  acc[channel] = messages[channel].map(({ author, date, ...rest }) => ({
     ...rest,
+    author: author.toLowerCase(),
     date: DateTime.fromFormat(date, 'yyyy-M-d h:m a')
   }));
   return acc;
