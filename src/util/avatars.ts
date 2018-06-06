8930cd25ecf7df29f6121ce5ac95bb1e02707e28
Alec Lownes <alec.lownes@gmail.com>
2018-06-06 18:25:34 -0400

    Add avatars

--- src/util/avatars.ts
+++ src/util/avatars.ts
@@ -1,11 +1,11 @@
 export const EMPTY_AVATAR = require('../../assets/images/empty.png');
 
 export const AVATARS: Record<string, string> = {
-  christy: EMPTY_AVATAR,
-  john: EMPTY_AVATAR,
-  anna: EMPTY_AVATAR,
-  david: EMPTY_AVATAR,
-  ajay: EMPTY_AVATAR,
-  pagerduty: EMPTY_AVATAR,
-  brad: EMPTY_AVATAR
+  christy: require('../../assets/images/christy.png'),
+  john: require('../../assets/images/john.png'),
+  anna: require('../../assets/images/anna.png'),
+  david: require('../../assets/images/david.png'),
+  ajay: require('../../assets/images/ajay.png'),
+  pagerduty: require('../../assets/images/pagerduty.png'),
+  brad: require('../../assets/images/brad.png')
 };
